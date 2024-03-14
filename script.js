12812496d5379eda9de5681084590e053156c5a5
Fagerberg <160026898+Fagerberg@users.noreply.github.com>
2024-03-14 14:52:22 +0100

    uppladdning efter handledning med patrick 14/3

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+
+    $("#toggleButton").click(function(){
+
+        $("#hiddenText").toggle(); // Visar eller döljer elementet
+
+    });
+
+    $("#toggleButton2").click(function(){
+
+        $("#hiddenText2").toggle(); // Visar eller döljer elementet
+
+    });
+
+
+});
+
+
